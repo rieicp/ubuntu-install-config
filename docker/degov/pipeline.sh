c4b5e07f486fc2f271f75b8034cc5fea2f9494d8
rieicp <35852894+rieicp@users.noreply.github.com>
2020-10-09 11:22:33 +0200

    new file pipeline.sh

---
+++ docker/degov/pipeline.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+composer install
+git apply "$BITBUCKET_CLONE_DIR/patches/modified-degov-for-testing-pipelines.patch"
+bash "$BITBUCKET_CLONE_DIR/docroot/profiles/contrib/degov/scripts/pipeline/acceptance_tests.sh" content install
+bash "$BITBUCKET_CLONE_DIR/docroot/profiles/contrib/degov/scripts/pipeline/acceptance_tests.sh" entities install
