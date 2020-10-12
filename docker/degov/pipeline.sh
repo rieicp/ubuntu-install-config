67a840d04441f4232ab4105f2cd2e6815eb62dd2
rieicp <35852894+rieicp@users.noreply.github.com>
2020-10-12 17:25:37 +0200

    Update pipeline.sh

--- docker/degov/pipeline.sh
+++ docker/degov/pipeline.sh
@@ -2,17 +2,14 @@
 
 ACTION=install
 
-cd "$BITBUCKET_CLONE_DIR/../"
 git clone https://gitlab.it.nrw.de/weini01/running-degov-tests.git
-rm -rf "$BITBUCKET_CLONE_DIR"
-mv -f running-degov-tests "$BITBUCKET_CLONE_DIR"
+mv -f running-degov-tests "$BITBUCKET_CLONE_DIR/project"
 
 
-
-ln -s "$BITBUCKET_CLONE_DIR/vendor/drush/drush/drush" /usr/local/bin/drush
-cd "$BITBUCKET_CLONE_DIR"
+ln -s "$BITBUCKET_CLONE_DIR/project/vendor/drush/drush/drush" /usr/local/bin/drush
+cd "$BITBUCKET_CLONE_DIR/project"
 composer install
-git apply "$BITBUCKET_CLONE_DIR/patches/modified-degov-for-testing-pipelines.patch"
+git apply "$BITBUCKET_CLONE_DIR/project/patches/modified-degov-for-testing-pipelines.patch"
 composer dump-autoload
-bash "$BITBUCKET_CLONE_DIR/docroot/profiles/contrib/degov/scripts/pipeline/acceptance_tests.sh" content $ACTION
-bash "$BITBUCKET_CLONE_DIR/docroot/profiles/contrib/degov/scripts/pipeline/acceptance_tests.sh" entities $ACTION
+bash "$BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/degov/scripts/pipeline/acceptance_tests.sh" content $ACTION
+bash "$BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/degov/scripts/pipeline/acceptance_tests.sh" entities $ACTION
