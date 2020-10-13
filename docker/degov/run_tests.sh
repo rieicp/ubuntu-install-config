59d304e249de5135e68806528f7231fdaf351105
rieicp <35852894+rieicp@users.noreply.github.com>
2020-10-13 11:44:31 +0200

    Create run_tests.sh

---
+++ docker/degov/run_tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+git clone https://gitlab.it.nrw.de/weini01/running-degov-tests.git
+mv -f running-degov-tests "$BITBUCKET_CLONE_DIR/project"
+
+ACTION="$1"
+
+ln -s "$BITBUCKET_CLONE_DIR/project/vendor/drush/drush/drush" /usr/local/bin/drush
+cd "$BITBUCKET_CLONE_DIR/project"
+composer install
+git apply "$BITBUCKET_CLONE_DIR/project/patches/modified-degov-for-testing-pipelines.patch"
+composer dump-autoload
+
+if [[ "install" == $ACTION ]]; then
+  $BITBUCKET_CLONE_DIR/project/vendor/behat/behat/bin/behat -c $BITBUCKET_CLONE_DIR/project/behat-no-drupal.dist.yml
+elif [[ "features" == $ACTION ]]; then
+  cd /opt/docker/ && unzip degov_project_DE_01.zip && cd -
+  drush sql-query --file="/opt/docker/degov_project_DE_01.sql" && rm -f /opt/docker/degov_project_DE_01.sql
+  $BITBUCKET_CLONE_DIR/project/vendor/behat/behat/bin/behat -c $BITBUCKET_CLONE_DIR/project/behat.dist.yml --tags="content"
+  $BITBUCKET_CLONE_DIR/project/vendor/behat/behat/bin/behat -c $BITBUCKET_CLONE_DIR/project/behat.dist.yml --tags="entities"
+fi
