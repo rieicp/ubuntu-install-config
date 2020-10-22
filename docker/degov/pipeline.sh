4f15802adb1c0f97c12e81c506746f81689a0253
rieicp <35852894+rieicp@users.noreply.github.com>
2020-10-22 11:57:16 +0200

    Update pipeline.sh

--- docker/degov/pipeline.sh
+++ docker/degov/pipeline.sh
@@ -4,9 +4,8 @@ FEATURE=$1
 ACTION=$2
 
 #git clone https://gitlab.it.nrw.de/weini01/running-degov-tests.git
-if [[ ! -d "$BITBUCKET_CLONE_DIR/project" ]]; then
-	mv -f running-degov-tests "$BITBUCKET_CLONE_DIR/project"
-fi
+rm -rf "$BITBUCKET_CLONE_DIR/project"
+mv -f running-degov-tests "$BITBUCKET_CLONE_DIR/project"
 
 ln -s "$BITBUCKET_CLONE_DIR/project/vendor/drush/drush/drush" /usr/local/bin/drush
 #cd "$BITBUCKET_CLONE_DIR/project"
@@ -14,7 +13,7 @@ ln -s "$BITBUCKET_CLONE_DIR/project/vendor/drush/drush/drush" /usr/local/bin/dru
 #git apply "$BITBUCKET_CLONE_DIR/project/patches/modified-degov-for-testing-pipelines.patch"
 #composer dump-autoload
 
-FILE=degov_project_DE_01.zip
+FILE=degov_project_DE_in_pipeline_just_installed.zip
 SQLFILE="$(echo $FILE | sed 's/.zip/.sql/')"
 #if [ ! -f "$BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/degov/testing/lfs_data/degov-stable-8.3.1.sql.gz" ]; then
   cp "/opt/docker/$FILE" "/tmp/$FILE"
