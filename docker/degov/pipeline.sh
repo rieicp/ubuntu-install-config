4abe69adfef52eb31131db1ca4a11e6339c8b513
rieicp <35852894+rieicp@users.noreply.github.com>
2020-10-29 11:52:03 +0100

    Update pipeline.sh

--- docker/degov/pipeline.sh
+++ docker/degov/pipeline.sh
@@ -15,7 +15,7 @@ cp "/opt/docker/$FILE" "/tmp/$FILE"
 cd /tmp
 unzip "/tmp/$FILE"
 mkdir -p $BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/base-gov/testing/lfs_data/
-mv -f "/tmp/$SQLFILE" $BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/base-gov/testing/lfs_data/degov-stable-8.3.1.sql
+mv -f "/tmp/$SQLFILE" $BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/base-gov/testing/lfs_data/base-gov-stable-8.3.1.sql
 rm -f "/tmp/$FILE" "/tmp/$SQLFILE"
 
 bash "$BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/base-gov/scripts/pipeline/acceptance_tests.sh" $FEATURE $ACTION
