b9146b70d95ea164e7c0c04d6ad10dcca7a990c0
rieicp <35852894+rieicp@users.noreply.github.com>
2020-11-03 13:25:24 +0100

    Updates

--- docker/degov/pipeline.sh
+++ docker/degov/pipeline.sh
@@ -8,17 +8,17 @@ if [[ -d $MY_PROJECT ]]; then
 	mv -f $MY_PROJECT "$BITBUCKET_CLONE_DIR/project"
 fi
 
-FILE=degov_project_DE_pipeline_install_install_00000.zip
+FILE=degov_project_DE_create_stable_db_dump.zip
 SQLFILE="$(echo $FILE | sed 's/.zip/.sql/')"
 
 cp "/opt/docker/$FILE" "/tmp/$FILE"
 cd /tmp
 unzip "/tmp/$FILE"
-mkdir -p $BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/base-gov/testing/lfs_data/
-mv -f "/tmp/$SQLFILE" $BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/base-gov/testing/lfs_data/base-gov-stable-8.3.1.sql
+mkdir -p $BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/freegov/testing/lfs_data/
+mv -f "/tmp/$SQLFILE" $BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/freegov/testing/lfs_data/freegov-stable-8.3.1.sql
 rm -f "/tmp/$FILE" "/tmp/$SQLFILE"
 cd -
 
-cd "$BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/" && ln -s base-gov degov && cd -
+cd "$BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/" && ln -s freegov degov && cd -
 
-bash "$BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/base-gov/scripts/pipeline/acceptance_tests.sh" $FEATURE $ACTION
+bash "$BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib/freegov/scripts/pipeline/acceptance_tests.sh" $FEATURE $ACTION
